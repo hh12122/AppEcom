d944395d57413cc7aea25813b728c4589d79cc03
hocine amrane <hocine.amrane@tech-instinct.com>
2024-10-28 00:43:49 +0100

    launch backend

---
+++ apps/Appecommerce-backend/src/main/java/dz/hocineprint/AppecommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package dz.hocineprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppecommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppecommerceBackendApplication.class, args);
+	}
+
+}
---
+++ apps/Appecommerce-backend/src/test/java/dz/hocineprint/AppecommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.hocineprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppecommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
